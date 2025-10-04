928cd09309f46affbfb74b2322bb8acdcbfd6287
Ben Pedigo <benjamindpedigo@gmail.com>
2025-10-03 17:14:16 -0700

    bumpversion

--- src/morphsync/__init__.py
+++ src/morphsync/__init__.py
@@ -2,11 +2,11 @@ import importlib.metadata
 
 __version__ = importlib.metadata.version("morphsync")
 
+from .base import Layer
 from .graph import Graph
 from .mesh import Mesh
 from .morph import MorphSync
 from .points import Points
 from .table import Table
-from .base import Layer
 
 __all__ = ["MorphSync", "Mesh", "Points", "Graph", "Table", "Layer"]
